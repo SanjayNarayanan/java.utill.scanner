d2f3112f97379d675743c8f7403a84c3fd922a62
SanjayNarayanan <112078568+SanjayNarayanan@users.noreply.github.com>
2023-12-07 16:11:18 +0530

    Tenth commit

---
+++ scannerclass.java
@@ -0,0 +1,11 @@
+package prtsc;
+import java.util.Scanner;
+
+class raid{
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter the values");
+        int a =input.nextInt();
+        System.out.println(a+5);
+    }
+}
